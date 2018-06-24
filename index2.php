b00f9366d543df74f1f1e5414941451bbcdcf70a
Jorge Gonzales Castillo <joryes1894@gmail.com>
2018-06-23 19:21:52 -0500

    aasfasf

--- index2.php
+++ index2.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" href="">
 </head>
 <body>
-	<h1>Pagina con GIT</h1>+
+	<p>ESTOS CAMBIOS SON DEL BRANCH BACKEND</p>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
